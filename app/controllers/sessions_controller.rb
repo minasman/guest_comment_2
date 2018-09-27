3224b80802b55cab723e286564e7c18cd873d9e3
Daniel Hernandez <elreymcd@yahoo.com>
2018-09-26 19:51:51 -0500

    add session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+    def welcome
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+        @user.try(:authenticate, params[:password])
+        if @user
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash.notice = "Username and/or Password Not Found"
+            redirect_to signin_path
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        redirect_to '/'
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash.notice = "Error Creating User"
+            render :new
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :username, :position, :email, :password)
+    end
+
 end
