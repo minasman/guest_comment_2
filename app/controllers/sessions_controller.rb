2b9b259db963c457156a1b0268e76e1a14b4acb7
Daniel Hernandez <elreymcd@yahoo.com>
2018-09-28 15:42:52 -0500

    updated to _form on user and store

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,7 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by(username: params[:user][:username])
-        @user.try(:authenticate, params[:password])
-        if @user
+        if @user.try(:authenticate, params[:user][:password])
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,12 @@ class UsersController < ApplicationController
     end
 
     def update
+        if @user.update(user_params)
+            redirect_to user_path(@user)
+        else
+            flash.notice = "Error Updating User"
+            render :edit
+        end
     end
 
     def destroy
