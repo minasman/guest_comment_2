2cd4de44ba6d824cf1fa4d3c0b76b450282a338f
Daniel Hernandez <elreymcd@yahoo.com>
2018-10-04 07:16:35 -0500

    more final touches

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -29,7 +29,7 @@ class SessionsController < ApplicationController
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
-            flash.notice = "Username and/or Password Not Found"
+            flash.alert = "Username and/or Password Not Found"
             redirect_to signin_path
         end
     end
